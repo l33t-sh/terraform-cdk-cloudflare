cb12d1e99e33940858b048b7e2a30ebaa286ebcc
NikolasLucansky <nikolas.lucansky@gmail.com>
2023-07-27 20:46:28 +0200

    Adding record

--- cloudflare/cloudflare.py
+++ cloudflare/cloudflare.py
@@ -1,10 +1,12 @@
 from constructs import Construct
-from cdktf import App, TerraformStack, S3Backend
+from cdktf import TerraformStack, S3Backend, Token
 
 from cdk_functions.get_config import get_config
 
 from cdktf_cdktf_provider_cloudflare.provider import CloudflareProvider
 from cdktf_cdktf_provider_cloudflare.data_cloudflare_zone import DataCloudflareZone
+from cdktf_cdktf_provider_cloudflare.zone import Zone
+from cdktf_cdktf_provider_cloudflare.record import Record, RecordData
 
 
 class CloudflareStack(TerraformStack):
@@ -13,8 +15,9 @@ class CloudflareStack(TerraformStack):
 
         config = get_config("config/config.yml")
 
-        cf_account_id=config['general']['cloudflare_account_id']
-        target_environment=config['general']['environment']
+        cf_account_id = config['general']['cloudflare_account_id']
+        target_environment = config['general']['environment']
+        null_value = Token.as_string(Token.null_value())
 
         CloudflareProvider(
             self,
@@ -44,13 +47,52 @@ class CloudflareStack(TerraformStack):
         )
 
         for zone in config['zones']:
-            domain_zone = DataCloudflareZone(
-                self,
-                "cf-zone-{}".format(
+            if zone['create_zone'] is True:
+                print("### | {} | Creating DNS Zone with name [{}]".format(
+                    id,
                     zone['name']
-                ),
-                account_id=cf_account_id,
-                name=zone['name'],
-            )
+                ))
+                domain_zone = Zone(
+                    self,
+                    "cf-zone-{}".format(
+                        zone['name']
+                    ),
+                    account_id=cf_account_id,
+                    zone=zone['name'],
+                )
+            else:
+                print("### | {} | Using DNS Zone with name [{}]".format(
+                    id,
+                    zone['name']
+                ))
+                domain_zone = DataCloudflareZone(
+                    self,
+                    "cf-zone-{}".format(
+                        zone['name']
+                    ),
+                    account_id=cf_account_id,
+                    name=zone['name'],
+                )
 
-
+            for record in zone['records']:
+                print("### | {} | Adding [{}] as [{}]".format(
+                    id,
+                    record['name'],
+                    record['type'],
+                ))
+                Record(
+                    self,
+                    "cf-record-{}-{}".format(
+                        zone['name'],
+                        record['name']
+                    ),
+                    name=record['name'],
+                    type=record['type'],
+                    zone_id=domain_zone.id,
+                    allow_overwrite=record['allow_overwrite'] if "allow_overwrite" in record else False,
+                    comment=record['comment'] if "comment" in record else None,
+                    priority=record['priority'] if "priority" in record else None,
+                    proxied=record['proxied'] if "proxied" in record else False,
+                    ttl=record['ttl'] if "ttl" in record else 1,
+                    value=record['value']
+                )
